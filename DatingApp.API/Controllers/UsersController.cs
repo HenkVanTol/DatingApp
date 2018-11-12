52766002ca700936cf5a0a5a35cf750772c03b31
henkvantol <henkjohanvantol@gmail.com>
2018-11-12 16:36:13 +1300

    Paging and filtering

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -9,58 +9,62 @@ using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DatingApp.API.Controllers
-{
-    [ServiceFilter(typeof(LogUserActivity))]
+namespace DatingApp.API.Controllers {
+    [ServiceFilter (typeof (LogUserActivity))]
     [Authorize]
-    [Route("api/[controller]")]
+    [Route ("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
-    {
+    public class UsersController : ControllerBase {
         private readonly IDatingRepository _repo;
         private readonly IMapper _mapper;
-        public UsersController(IDatingRepository repo, IMapper mapper)
-        {
+        public UsersController (IDatingRepository repo, IMapper mapper) {
             _mapper = mapper;
             _repo = repo;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
-        {
-            var users = await _repo.GetUsers(userParams);
+        public async Task<IActionResult> GetUsers ([FromQuery] UserParams userParams) {
+            var currentUserId = int.Parse (User.FindFirst (ClaimTypes.NameIdentifier).Value);
 
-            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+            var userFromRepo = await _repo.GetUser (currentUserId);
 
-            Response.AddPagination(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
+            userParams.UserId = currentUserId;
 
-            return Ok(usersToReturn);
+            if (string.IsNullOrEmpty (userParams.Gender)) {
+                userParams.Gender = userFromRepo.Gender == "male" ? "female" : "male";
+            }
+
+            var users = await _repo.GetUsers (userParams);
+
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>> (users);
+
+            Response.AddPagination (users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
+
+            return Ok (usersToReturn);
         }
 
-        [HttpGet("{id}", Name = "GetUser")]
-        public async Task<IActionResult> GetUser(int id)
-        {
-            var user = await _repo.GetUser(id);
+        [HttpGet ("{id}", Name = "GetUser")]
+        public async Task<IActionResult> GetUser (int id) {
+            var user = await _repo.GetUser (id);
 
-            var userToReturn = _mapper.Map<UserForDetailedDto>(user);
+            var userToReturn = _mapper.Map<UserForDetailedDto> (user);
 
-            return Ok(userToReturn);
+            return Ok (userToReturn);
         }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
-        {
-            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
-                return Unauthorized();
+        [HttpPut ("{id}")]
+        public async Task<IActionResult> UpdateUser (int id, UserForUpdateDto userForUpdateDto) {
+            if (id != int.Parse (User.FindFirst (ClaimTypes.NameIdentifier).Value))
+                return Unauthorized ();
 
-            var userFromRepo = await _repo.GetUser(id);
+            var userFromRepo = await _repo.GetUser (id);
 
-            _mapper.Map(userForUpdateDto, userFromRepo);
+            _mapper.Map (userForUpdateDto, userFromRepo);
 
-            if (await _repo.SaveAll())
-                return NoContent();
+            if (await _repo.SaveAll ())
+                return NoContent ();
 
-            throw new Exception($"Updating user {id} failed on save");
+            throw new Exception ($"Updating user {id} failed on save");
         }
     }
 }
